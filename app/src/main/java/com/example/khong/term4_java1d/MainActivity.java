5965fa64d2709f3736d66927dab3a008c4a95e3e
jin kind <jinkind_ho@mymail.sutd.edu.sg>
2018-11-17 14:16:49 +0800

    171118_1416 intent btn pages

--- app/src/main/java/com/example/khong/term4_java1d/MainActivity.java
+++ app/src/main/java/com/example/khong/term4_java1d/MainActivity.java
@@ -17,11 +17,11 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_washer:
+                    //mTextMessage.setText(R.string.title_wa);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_dryer:
+                    //mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
