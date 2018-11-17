e8c2b2e92277c67aa3e28f42c6c3a4c68e462a57
jiaweikhong <khongjw@gmail.com>
2018-11-17 16:34:14 +0800

    Tried but failed shinebutton

--- app/src/main/java/com/example/khong/term4_java1d/MainActivity.java
+++ app/src/main/java/com/example/khong/term4_java1d/MainActivity.java
@@ -5,15 +5,22 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.sackcentury.shinebuttonlib.ShineButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    ShineButton goToWashersImgBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        goToWashersImgBtn = (ShineButton) findViewById(R.id.po_image2);
+        goToWashersImgBtn.init(this);
     }
 
 }
