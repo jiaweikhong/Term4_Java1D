bb2f071921a9f2fb4e5e449ac13acb3d6657bea0
jin kind <jinkind_ho@mymail.sutd.edu.sg>
2018-11-20 02:05:03 +0800

    sync shinebuttons

--- app/src/main/java/com/example/khong/term4_java1d/MainActivity.java
+++ app/src/main/java/com/example/khong/term4_java1d/MainActivity.java
@@ -22,11 +22,18 @@ public class MainActivity extends AppCompatActivity {
     private CardView gotoDryersImgBtn;
     private ShineButton washersNotifAllImgBtn;
     private ShineButton dryersNotifAllImgBtn;
+    private TextView washerNotifState;
+    private  TextView dryerNotifState;
+    private boolean washerNotifStatus= false;
+    private boolean dryerNotifStatus=true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        washerNotifState = findViewById(R.id.washerNotifState);
+        dryerNotifState = findViewById(R.id.dryerNotifState);
+
         gotoWashersImgBtn = findViewById(R.id.goToWashers);
         gotoWashersImgBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,15 +56,43 @@ public class MainActivity extends AppCompatActivity {
         washersNotifAllImgBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "You will now be notified when next washer is available", Toast.LENGTH_LONG).show();
-                //TODO move to strings.xml
+                if(washersNotifAllImgBtn.isChecked()) {
+                    Toast.makeText(MainActivity.this, "You will now be notified when next washer is available", Toast.LENGTH_LONG).show();
+                    washerNotifState.setText(R.string.notification_enabled);
+                }//TODO move to strings.xml
+                //
+                else{
+                Toast.makeText(MainActivity.this, "You have remove notification for washer", Toast.LENGTH_LONG).show();
+                    washerNotifState.setText(R.string.notification_disabled);
+            }
             }
         });
         dryersNotifAllImgBtn = findViewById(R.id.dryersNotifAllImgBtn);
+        if(dryerNotifStatus){
+            dryerNotifState.setText(R.string.notification_unavailable);
+            dryersNotifAllImgBtn.setBtnFillColor(R.color.colorPrimary);
+            dryersNotifAllImgBtn.setShapeResource(R.drawable.ic_assets_disabledbell);
+            dryersNotifAllImgBtn.setChecked(true);
+        }
         dryersNotifAllImgBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "You will now be notified when next dryer is available", Toast.LENGTH_LONG).show();
+                if(dryerNotifStatus){
+                    dryersNotifAllImgBtn.setChecked(true);
+
+                    Toast.makeText(MainActivity.this, "There are available dryers", Toast.LENGTH_LONG).show();
+
+                }
+                if(dryersNotifAllImgBtn.isChecked()&&!dryerNotifStatus) {
+
+                    Toast.makeText(MainActivity.this, "You will now be notified when next dryer is available", Toast.LENGTH_LONG).show();
+                    dryerNotifState.setText(R.string.notification_enabled);
+                }
+                else if(!dryerNotifStatus){
+                    Toast.makeText(MainActivity.this, "You have remove notification for dryer", Toast.LENGTH_LONG).show();
+
+                    dryerNotifState.setText(R.string.notification_disabled);
+                }
             }
         });
 
