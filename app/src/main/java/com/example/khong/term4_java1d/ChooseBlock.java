fc5f540d980e65454efd6a3eeb272c73d5a3cab1
Timothy Liu <tlkh.xms@gmail.com>
2018-11-25 02:19:36 +0800

    Nuclear commit: added firebase login and basic notifications

--- app/src/main/java/com/example/khong/term4_java1d/ChooseBlock.java
+++ app/src/main/java/com/example/khong/term4_java1d/ChooseBlock.java
@@ -1,11 +1,16 @@
 package com.example.khong.term4_java1d;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class ChooseBlock extends AppCompatActivity {
 
@@ -25,9 +30,7 @@ public class ChooseBlock extends AppCompatActivity {
         blk55Card.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO: store value 55 into user's firebase?
-                Toast.makeText(ChooseBlock.this, "You have chosen Block 55", Toast.LENGTH_LONG).show();
-
+                setUserBlock(55);
                 Intent intent = new Intent(ChooseBlock.this, MainActivity.class);   // Go to main activity
                 startActivity(intent);
             }
@@ -36,8 +39,7 @@ public class ChooseBlock extends AppCompatActivity {
         blk57Card.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO: store value 57 into user's firebase?
-                Toast.makeText(ChooseBlock.this, "You have chosen Block 57", Toast.LENGTH_LONG).show();
+                setUserBlock(57);
                 Intent intent = new Intent(ChooseBlock.this, MainActivity.class);   // Go to main activity
                 startActivity(intent);
             }
@@ -46,11 +48,62 @@ public class ChooseBlock extends AppCompatActivity {
         blk59Card.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO: store value 59 into user's firebase?
-                Toast.makeText(ChooseBlock.this, "You have chosen Block 59", Toast.LENGTH_LONG).show();
+                setUserBlock(59);
                 Intent intent = new Intent(ChooseBlock.this, MainActivity.class);   // Go to main activity
                 startActivity(intent);
             }
         });
+    }
+
+    protected void setUserBlock(int block) {
+        switch(block) {
+            case 55:
+                    subscribeTopic("block_55");
+                    unsubscribeTopic("block_57");
+                    unsubscribeTopic("block_59");
+                break;
+            case 57:
+                    unsubscribeTopic("block_55");
+                    subscribeTopic("block_57");
+                    unsubscribeTopic("block_59");
+                break;
+            case 59:
+                    unsubscribeTopic("block_55");
+                    unsubscribeTopic("block_57");
+                    subscribeTopic("block_59");
+                break;
+        }
+        Toast.makeText(ChooseBlock.this, "You have chosen: Block "+block, Toast.LENGTH_SHORT).show();
+    }
+
+
+    protected void subscribeTopic(final String topic_name) {
+        FirebaseMessaging.getInstance().subscribeToTopic(topic_name)
+                // unsub with unsubscribeFromTopic()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        String msg = "Subscribed to "+topic_name;
+                        if (!task.isSuccessful()) {
+                            msg = "Subscribe failed";
+                        }
+                        Toast.makeText(ChooseBlock.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    protected void unsubscribeTopic(final String topic_name) {
+        FirebaseMessaging.getInstance().unsubscribeFromTopic(topic_name)
+                // unsub with unsubscribeFromTopic()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        String msg = "Unsubscribed from "+topic_name;
+                        if (!task.isSuccessful()) {
+                            msg = "Unsubscribe failed";
+                        }
+                        Toast.makeText(ChooseBlock.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
