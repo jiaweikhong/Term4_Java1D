a0b07b18a00bcc06e96adfd2b62daaca382e52eb
jiaweikhong <khongjw@gmail.com>
2018-12-05 15:15:28 +0800

    updated statistics activty and xml

--- app/src/main/java/com/example/khong/term4_java1d/ChartingFiles/PieChartItem.java
+++ app/src/main/java/com/example/khong/term4_java1d/ChartingFiles/PieChartItem.java
@@ -89,13 +89,13 @@ public class PieChartItem extends ChartItem {
     }
 
     private SpannableString generateCenterText() {
-        SpannableString s = new SpannableString("MPAndroidChart\ncreated by\nPhilipp Jahoda");
-        s.setSpan(new RelativeSizeSpan(1.6f), 0, 14, 0);
-        s.setSpan(new ForegroundColorSpan(ColorTemplate.VORDIPLOM_COLORS[0]), 0, 14, 0);
-        s.setSpan(new RelativeSizeSpan(.9f), 14, 25, 0);
-        s.setSpan(new ForegroundColorSpan(Color.GRAY), 14, 25, 0);
-        s.setSpan(new RelativeSizeSpan(1.4f), 25, s.length(), 0);
-        s.setSpan(new ForegroundColorSpan(ColorTemplate.getHoloBlue()), 25, s.length(), 0);
+        SpannableString s = new SpannableString("Your machine usage\n2\ntimes a week");
+        s.setSpan(new RelativeSizeSpan(1.4f), 0, 19, 0);
+        s.setSpan(new ForegroundColorSpan(ColorTemplate.VORDIPLOM_COLORS[0]), 0, 19, 0);
+        s.setSpan(new RelativeSizeSpan(2.0f), 19, 20, 0);
+        s.setSpan(new ForegroundColorSpan(Color.GRAY), 19, 20, 0);
+        s.setSpan(new RelativeSizeSpan(1.4f), 20, s.length(), 0);
+        s.setSpan(new ForegroundColorSpan(ColorTemplate.getHoloBlue()), 20, s.length(), 0);
         return s;
     }
 
